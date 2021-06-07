2f049bacc40aef93fb77850da91b36fe53b1147e
Dohun Ryu <tbc20118607@cu.ac.kr>
2021-06-07 13:34:38 +0900

    commit

--- 자료구조/소스.cpp
+++ 자료구조/소스.cpp
@@ -9,60 +9,77 @@
 typedef struct graphNode {
 	int vertex;
 	struct graphNode* link;
-} graphNode;
+}graphNode;
 
 typedef struct graphType {
 	int n;								// 정점 개수
 	graphNode* adjList_H[MAX_VERTEX];	// 정점에 대한 인접 리스트의 헤드 노드 배열
 	int visited[MAX_VERTEX];			// 정점에 대한 방문 표시를 위한 배열
-} graphType;
+}graphType;
 
-// << 스택 (5장에서 설명한 연결 리스트를 이용한 스택 연산 수행:[예제 5-2]의 05~43행과 동일)
 typedef int element;
 
-typedef struct stackNode {
+// << 큐 (6장에서 설명한 연결 리스트를 이용한 큐 연산 수행:[예제 6-3]의 05~60행과 동일)
+typedef struct QNode {
 	int data;
-	struct stackNode* link;
-} stackNode;
+	struct QNode* link;
+} QNode;
 
-stackNode* top;
+typedef struct {
+	QNode* front, * rear;
+} LQueueType;
 
-// 스택이 공백인지 확인하는 연산
-int isEmpty() {
-	if (top == NULL) return 1;
+LQueueType* createLinkedQueue() {
+	LQueueType* LQ;
+	LQ = (LQueueType*)malloc(sizeof(LQueueType));
+	LQ->front = NULL;
+	LQ->rear = NULL;
+	return LQ;
+}
+
+int isEmpty(LQueueType* LQ) {
+	if (LQ->front == NULL) {
+		printf("\n Linked Queue is empty! \n");
+		return 1;
+	}
 	else return 0;
 }
 
-void push(int item) {
-	stackNode* temp = (stackNode*)malloc(sizeof(stackNode));
-	temp->data = item;
-	temp->link = top;
-	top = temp;
-}
-
-int pop() {
-	int item;
-	stackNode* temp = top;
-
-	if (isEmpty()) {
-		printf("\n\n Stack is empty !\n");
-		return 0;
+void enQueue(LQueueType* LQ, int item) {
+	QNode* newNode = (QNode*)malloc(sizeof(QNode));
+	newNode->data = item;
+	newNode->link = NULL;
+	if (LQ->front == NULL) {
+		LQ->front = newNode;
+		LQ->rear = newNode;
 	}
 	else {
-		item = temp->data;
-		top = temp->link;
-		free(temp);
+		LQ->rear->link = newNode;
+		LQ->rear = newNode;
+	}
+}
+
+int deQueue(LQueueType* LQ) {
+	QNode* old = LQ->front;
+	int item;
+	if (isEmpty(LQ)) return 0;
+	else {
+		item = old->data;
+		LQ->front = LQ->front->link;
+		if (LQ->front == NULL)
+			LQ->rear = NULL;
+		free(old);
 		return item;
 	}
-} // 스택 >>
+} // 큐 >>
 
-// 깊이 우선 탐색을 위해 초기 공백 그래프를 생성하는 연산
+// 너비 우선 탐색을 위한 초기 공백 그래프를 생성하는 연산
 void createGraph(graphType* g) {
 	int v;
-	g->n = 0;							// 그래프의 정점 개수를 0으로 초기화
+	g->n = 0;						// 그래프의 정점 개수를 0으로 초기화
 	for (v = 0; v < MAX_VERTEX; v++) {
-		g->visited[v] = FALSE;			// 그래프의 정점에 대한 배열 visited를 FALSE로 초기화
-		g->adjList_H[v] = NULL;			// 인접 리스트에 대한 헤드 노드 배열을 NULL로 초기화
+		g->visited[v] = FALSE;		// 그래프 정점에 대한 배열 visited를 FALSE로 초기화
+		g->adjList_H[v] = NULL;		// 인접 리스트에 대한 헤드 노드 배열을 NULL로 초기화
 	}
 }
 
@@ -88,12 +105,12 @@ void insertEdge(graphType* g, int u, int v) {
 	g->adjList_H[u] = node;
 }
 
-// 그래프 g의 각 정점에 대한 인접 리스트를 출력하는 연산 : [예제 8-2]의 50~61행과 동일
+// 그래프 g에 대한 인접 리스트를 출력하는 연산 : [예제 8-2]의 50~61행과 동일
 void print_adjList(graphType* g) {
 	int i;
 	graphNode* p;
 	for (i = 0; i < g->n; i++) {
-		printf("\n\t\t정점%c의 인접 리스트", i + 65);
+		printf("\n\t\t정점 %c의 인접 리스트", i + 65);
 		p = g->adjList_H[i];
 		while (p) {
 			printf(" -> %c", p->vertex + 65);
@@ -102,32 +119,26 @@ void print_adjList(graphType* g) {
 	}
 }
 
-// 그래프 g에서 정점 v에 대한 깊이 우선 탐색 연산
-void DFS_adjList(graphType* g, int v) {
+// 그래프 g에서 정점 v에 대한 너비 우선 탐색 연산
+void BFS_adjList(graphType* g, int v) {
 	graphNode* w;
-	top = NULL;					// 스택의 top 설정
-	push(v);					// 깊이 우선 탐색을 시작하는 정점 v를 스택에 push
-	g->visited[v] = TRUE;		// 정점 v를 방문했으므로 해당 배열 값을 TRUE로 설정 
+	LQueueType* Q;				// 큐
+	Q = createLinkedQueue();	// 큐 생성
+	g->visited[v] = TRUE;		// 시작 정점 v에 대한 배열 값을 TRUE로 설정
 	printf(" %c", v + 65);
+	enQueue(Q, v);			// 현재 정점 v를 큐에 enQueue
 
-	// 스택이 공백이 아닌 동안 깊이 우선 탐색 반복
-	while (!isEmpty()) {
-		w = g->adjList_H[v];
-		// 인접 정점이 있는 동안 수행
-		while (w) {
-			// 현재 정점 w를 방문하지 않은 경우 
-			if (!g->visited[w->vertex]) {
-				push(w->vertex);					// 현재 정점 W를 스택에 push
-				g->visited[w->vertex] = TRUE;	// 정점 w에 대한 배열 값을 TRUE로 설정
-				printf(" %c", w->vertex + 65);	// 정점 0~6을 A~G로 바꾸어서 출력
-				v = w->vertex;
-				w = g->adjList_H[v];
+	// 큐가 공백인 아닌 동안 너비 우선 탐색 수행
+	while (!isEmpty(Q)) {
+		v = deQueue(Q);
+		// 현재 정점 w를 방문하지 않은 경우 
+		for (w = g->adjList_H[v]; w; w = w->link)	// 인접 정점이 있는 동안 수행
+			if (!g->visited[w->vertex]) {	// 정점 w가 방문하지 않은 정점인 경우
+				g->visited[w->vertex] = TRUE;
+				printf(" %c", w->vertex + 65); // 정점 0~6을 A~G로 바꾸어 출력
+				enQueue(Q, w->vertex);
 			}
-			// 현재 정점 w가 이미 방문된 경우
-			else w = w->link;
-		}
-		v = pop();// 현재 정점에서 순회를 진행할 인접 정점이 더 없는 경우에 스택 pop!
-	} // 스택이 공백이면 깊이 우선 탐색 종료
+	} // 큐가 공백이면 너비 우선 탐색 종료
 }
 
 void main() {
@@ -158,8 +169,8 @@ void main() {
 	printf("\n G9의 인접 리스트 ");
 	print_adjList(G9);
 
-	printf("\n\n///////////////\n\n깊이 우선 탐색 >> ");
-	DFS_adjList(G9, 0);	// 0번 정점인 정점 A에서 깊이 우선 탐색 시작
+	printf("\n\n///////////////\n\n너비 우선 탐색 >> ");
+	BFS_adjList(G9, 0);      // 0번 정점인 정점 A에서 너비 우선 탐색 시작
 
 	getchar();
 }
